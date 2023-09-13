770270e8a91f9aa04e5ac53f55b6bdef41e793a1
Luis Pérez <ljpm9401@gmail.com>
2023-09-13 14:48:41 -0500

    Ajustes

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,18 +8,9 @@
           i.fas.fa-info
       h1 Introducción
 
-    .bloque-texto-g.color-acento-contenido.p-3.p-sm-4.p-md-5
-      .bloque-texto-g__img(
-        :style="{'background-image': `url(${require('@/assets/curso/intro1.png')})`}"
-      )
-      .bloque-texto-g__texto.p-4
-        p.mb-0 Las droguerías y farmacias-droguerías son establecimientos farmacéuticos que se han ido actualizando en busca de satisfacer las necesidades de pacientes, clientes y usuarios, por lo cual, ya no solo se consiguen medicamentos y dispositivos médicos, sino una alta gama de productos de bienestar en general.
-        br
-        br
-        | Actualmente, estos establecimientos han incorporado en sus intereses el #[em marketing] farmacéutico de modo que puedan suplir las necesidades del consumidor, pero también hacer crecer y sostener el negocio.
-        br
-        br
-        | A través de este componente formativo conocerás diferentes temas relacionados con la exhibición de los productos farmacéuticos como elemento clave de la estrategia comercial del #[em marketing] farmacéutico. 
+    figure
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     
  
